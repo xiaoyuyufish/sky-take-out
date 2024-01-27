88af7db1fb6dbd39a53e9434834f15d0bbccf1bb
xiaoyuyufish <1270934326@qq.com>
2024-01-27 21:48:00 +0800

    文件上传

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,40 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import jdk.internal.foreign.ResourceScopeImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.UUID;
+
+@RequestMapping("/admin/common")
+@RestController
+@Slf4j
+public class CommonController {
+
+    @Resource
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String objectName = UUID.randomUUID() + extension;
+
+        try {
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.info("文件上传失败");
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
