1ed1116786f56a6f0703e27ef59d6dea050e8a8e
xiaoyuyufish <1270934326@qq.com>
2024-02-05 10:08:36 +0800

    提交订单

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package com.sky.mapper;
 import com.sky.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     User getByOpenId(String openid);
 
     void insert(User user);
+
+    @Select("select * from sky_take_out.user where id = #{userId}")
+    User getById(Long userId);
 }
